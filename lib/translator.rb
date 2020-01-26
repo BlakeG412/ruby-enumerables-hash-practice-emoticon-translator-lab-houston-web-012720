51ee55efd5f01b959d27be59fcb1e2bcfbef6290
Blake Gaal <bag.blakeandrewgaal@gmail.com>
2020-01-26 02:25:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,6 +24,7 @@ def get_japanese_emoticon
 end
 
 def get_english_meaning
-  
+  result = load_library(file_path)['get_meaning'][emoticon]
+  result ? result : "Sorry, that emoticon was not found"
   # code goes here
 end
